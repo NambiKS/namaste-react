78481a744dea8a2da10fd0956c7037807093b922
NambiKS <nambiksomaiya@gmail.com>
2023-08-22 11:29:03 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
-
-/*
-
-<div id="parent">
-    <div id="child">
-        <h1> I am an H1 Tag </h1>
-        <h2> I am an H2 Tag </h2>
-    </div>
-    <div id="child2">
-        <h1> I am an H1 Tag </h1>
-        <h2> I am an H2 Tag </h2>
-    </div>
-</div>
-
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    { id: "parent" }, 
-    [
-        React.createElement(
-            "div", 
-            { id: "child" }, [
-            React.createElement("h1", {}, "I am an H1 Tag"), React.createElement("h2",{},"I am an H2 Tag")
-        ]),
-        React.createElement(
-            "div", 
-            { id: "child2" }, [
-            React.createElement("h1", {}, "I am an H1 Tag"), React.createElement("h2",{},"I am an H2 Tag")
-        ])
-    ]
+// React Element
+const jsxHeading = (
+  <h1 className="heading" tabIndex="5">
+    Namaste React!
+  </h1>
 );
+
+// React Component
+
+const HeadingComponent = () => {
+  return (
+    <h1 className="heading" tabIndex="5">
+      Namaste React from functional component 1
+    </h1>
+  );
+};
+
+
+//  COMPONENT Composition
+const HeadingComponent2 = () => (
+  <div className="container">
+    <HeadingComponent />
+    <h1 className="heading" tabIndex="5">
+      Namaste React from functional component 2
+    </h1>
+  </div>
+);
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent2 />);
